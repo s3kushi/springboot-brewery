e9a8ea90ae7ca659a960835dfcee1e8cd92f686a
s3kushi <daniel.756@hotmail.com>
2021-02-06 23:49:29 -0300

    initial commit

---
+++ src/test/java/com/sekushi/springbootbrewery/SpringbootBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sekushi.springbootbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

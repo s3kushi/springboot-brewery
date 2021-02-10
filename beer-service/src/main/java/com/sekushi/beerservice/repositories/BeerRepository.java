398337a8b7fa027f584bed6c5a6a145643625f79
s3kushi <daniel.756@hotmail.com>
2021-02-10 02:08:21 -0300

    Beer entity and repository.

---
+++ beer-service/src/main/java/com/sekushi/beerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.sekushi.beerservice.repositories;
+
+import com.sekushi.beerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}

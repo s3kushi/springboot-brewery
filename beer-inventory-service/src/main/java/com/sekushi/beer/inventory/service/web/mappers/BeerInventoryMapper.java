571eef617d63226c9ee7fad182d9dd6b8de47987
s3kushi <daniel.756@hotmail.com>
2021-02-11 16:21:36 -0300

    Beer inventory service.

---
+++ beer-inventory-service/src/main/java/com/sekushi/beer/inventory/service/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,16 @@
+package com.sekushi.beer.inventory.service.web.mappers;
+
+import com.sekushi.beer.inventory.service.domain.BeerInventory;
+import com.sekushi.beer.inventory.service.web.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by jt on 2019-05-31.
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
